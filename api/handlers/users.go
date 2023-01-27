c014c636e1f3bff9b1b6372b2915296919d87f8d
Phanor Coll <phanorcoll@gmail.com>
2023-01-27 13:27:46 -0400

    📝 Updated User handler
    Cleanup the code.
    
    👷 Implements-Requirement: #2

--- api/handlers/users.go
+++ api/handlers/users.go
@@ -123,30 +123,27 @@ func UpdateUser(c echo.Context) error {
 		return c.JSON(http.StatusNotFound, "user not found")
 	}
 	currentUser := UserPublic{}
-	_ = json.Unmarshal(v, &currentUser)
+	err = json.Unmarshal(v, &currentUser)
+	if err != nil {
+		log.Fatalf("Cannot Marshal currentUser %s\n", err)
+	}
 
-	//Get the new values from the body
-	// body := UserPublic{}
 	defer c.Request().Body.Close()
+
+	//Get the body from the request and replace the content of currentUser
 	err = json.NewDecoder(c.Request().Body).Decode(&currentUser)
 	if err != nil {
 		log.Fatalf("Failed reading the request body %s\n", err)
 		return c.JSON(http.StatusInternalServerError, err.Error)
 	}
-
-	u, err := json.Marshal(currentUser)
+	mCurrentUser, err := json.Marshal(currentUser)
 	if err != nil {
 		log.Fatalf("Cannot Marshal User %s\n", err)
 	}
 
-
-	if err := DB.Set([]byte(currentUser.Id), []byte(u)); err != nil {
+	if err := DB.Set([]byte(currentUser.Id), []byte(mCurrentUser)); err != nil {
 		log.Fatal(err)
 	}
-	// err = DB.DB.Update(func(txn *badger.Txn) error {
-
-	// 	return nil
-	// })
 
 	return c.JSON(http.StatusOK, currentUser)
 }
